063cd016425ef09380365f81b9565abfb19bab8e
andrewkss02 <andrewkss02@gmail.com>
2022-10-18 21:59:40 +0700

    UTS

---
+++ app/src/main/java/com/cotlin/utskalkulator/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.cotlin.utskalkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun kali (view:View){
+        val input1 :EditText =findViewById(R.id.input1)
+        val input2 :EditText =findViewById(R.id.input2)
+        val hasil :TextView =findViewById(R.id.hasil)
+        hasil.text = (input1.text.toString().toInt() * input2.text.toString().toInt()).toString()
+    }
+    fun tambah (view: View){
+        val input1 :EditText =findViewById(R.id.input1)
+        val input2 :EditText =findViewById(R.id.input2)
+        val hasil :TextView =findViewById(R.id.hasil)
+        hasil.text = (input1.text.toString().toInt() + input2.text.toString().toInt()).toString()
+    }
+}
